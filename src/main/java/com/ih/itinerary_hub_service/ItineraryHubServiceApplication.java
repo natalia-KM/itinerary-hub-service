7d519f69a989d05ca79d4d9fa35aca641eabf80e
Nat <nkmiecik88@gmail.com>
2025-03-05 19:40:13 +0000

    Initial commit

---
+++ src/main/java/com/ih/itinerary_hub_service/ItineraryHubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ih.itinerary_hub_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItineraryHubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItineraryHubServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ih/itinerary_hub_service/ItineraryHubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ih.itinerary_hub_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItineraryHubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
