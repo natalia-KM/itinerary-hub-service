8388fca5b883762a973b506e3fea3a335b5a0d0b
Nat <nkmiecik88@gmail.com>
2025-03-06 01:02:17 +0000

    empty commit

--- src/main/java/com/ih/itinerary_hub_service/ItineraryHubServiceApplication.java
+++ src/main/java/com/ih/itinerary_hub_service/ItineraryHubServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ItineraryHubServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("SOUT");
 		SpringApplication.run(ItineraryHubServiceApplication.class, args);
 	}
 
