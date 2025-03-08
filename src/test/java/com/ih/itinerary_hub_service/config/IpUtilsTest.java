c2e592952f21f53887b8b441d13742f008fd94be
Nat <nkmiecik88@gmail.com>
2025-03-08 02:44:08 +0000

    first working test + valid deps

---
+++ src/test/java/com/ih/itinerary_hub_service/config/IpUtilsTest.java
@@ -0,0 +1,31 @@
+package com.ih.itinerary_hub_service.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class IpUtilsTest {
+
+
+    @Test
+    void hasIpAddress_shouldReturnTrue_whenIpMatches() throws NullPointerException {
+        HttpServletRequest mockRequest = mock(HttpServletRequest.class);
+        when(mockRequest.getRemoteAddr()).thenReturn("192.168.1.1");
+
+        RequestAuthorizationContext mockContext = mock(RequestAuthorizationContext.class);
+        when(mockContext.getRequest()).thenReturn(mockRequest);
+
+        AuthorizationManager<RequestAuthorizationContext> authorizationManager = IpUtils.hasIpAddress("192.168.1.1");
+
+        boolean isValid = authorizationManager.authorize(null, mockContext).isGranted();
+
+        assertTrue(isValid, "The IP address should match.");
+
+    }
+}
