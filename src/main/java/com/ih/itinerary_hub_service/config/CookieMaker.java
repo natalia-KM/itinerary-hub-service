971c480eb1fc491b1a8f55d508c49ab068bbdf74
Natalia <76484753+natalia-KM@users.noreply.github.com>
2025-03-16 17:45:58 +0000

    Feat/db (#21)
    Skipping workflow as its urgent

--- src/main/java/com/ih/itinerary_hub_service/config/CookieMaker.java
+++ src/main/java/com/ih/itinerary_hub_service/config/CookieMaker.java
@@ -38,11 +38,13 @@ public class CookieMaker {
     public Cookie makeCookie(String cookieName, String cookieValue) {
         boolean isEnabled = cookieProperties.isEnabled();
 
+        String sameSite = isEnabled ? "None" : "Lax";
+
         Cookie accessTokenCookie = new Cookie(cookieName, cookieValue);
         accessTokenCookie.setHttpOnly(true);
         accessTokenCookie.setSecure(isEnabled);
         accessTokenCookie.setPath("/");
-        accessTokenCookie.setAttribute("SameSite", "Lax");
+        accessTokenCookie.setAttribute("SameSite", sameSite);
 
         return accessTokenCookie;
     }
