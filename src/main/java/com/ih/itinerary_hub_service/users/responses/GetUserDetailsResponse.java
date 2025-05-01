6e9460f46188f0beb0242ef7e58a51228b932ab5
Natalia <76484753+natalia-KM@users.noreply.github.com>
2025-05-01 15:54:57 +0100

    feat: return createdAt in user details response (#38)

--- src/main/java/com/ih/itinerary_hub_service/users/responses/GetUserDetailsResponse.java
+++ src/main/java/com/ih/itinerary_hub_service/users/responses/GetUserDetailsResponse.java
@@ -1,6 +1,13 @@
 package com.ih.itinerary_hub_service.users.responses;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-public record GetUserDetailsResponse(String firstName, String lastName, boolean isGuest, Optional<String> currency) {
+public record GetUserDetailsResponse(
+        String firstName,
+        String lastName,
+        boolean isGuest,
+        LocalDateTime createdAt,
+        Optional<String> currency
+) {
 }
